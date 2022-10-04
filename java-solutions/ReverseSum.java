f6c1fcaacf8f2443d3f2da0c5f22cf1ec5119128
Aleksey Chiryatev <aechiryatev@gmail.com>
2022-10-04 12:01:56 +0300

    some changes

--- java-solutions/ReverseSum.java
+++ java-solutions/ReverseSum.java
@@ -13,16 +13,14 @@ public class ReverseSum{
             return brr;
         }
         else{
-
             arr[arr[arr.length - 1]] = n;
             arr[arr.length - 1] += 1;
             return arr;
         }
     }
 
-
     public static void main(String[] args) {
-        ArrayList<int[]> f = new ArrayList<>();
+        List<int[]> f = new ArrayList<>();
         Scanner scanLines = new Scanner(System.in);
         int maxLen = 0;
         while (scanLines.hasNextLine()) {
@@ -36,7 +34,6 @@ public class ReverseSum{
                 }
                 maxLen = Math.max(n[n.length - 1], maxLen);
                 f.add(n);
-                
             } else {
                 f.add(new int[0]);
             }
@@ -60,59 +57,5 @@ public class ReverseSum{
             }
             System.out.println();
         }
-        /*
-        int[] numbers = new int[1_000_000];
-        int[] lines = new int[1_000_000];
-
-        Scanner scanLines = new Scanner(System.in);
-        int lineCount = 0;
-        int lineIndex = 0;
-        int maxLen = 0;
-        while (scanLines.hasNextLine()) {
-            String line = scanLines.nextLine();
-            Scanner ints = new Scanner(line);
-            lines[lineCount] = lineIndex;
-            int len = 0;
-            while (ints.hasNextInt()) {
-                numbers[lineIndex] = ints.nextInt();
-                //System.err.print(numbers[lineIndex] + " ");
-                lineIndex++;
-                len++;
-            }
-            //System.err.println();
-            maxLen = Math.max(len, maxLen);
-            len = 0;
-            lineCount++;
-        }
-        lines[lineCount] = lineIndex;
-        int[] linesSum = new int[lineCount];
-        int[] columnSum = new int[maxLen];
-        for (int i = 0; i < lineCount; i++) {
-            for (int j = lines[i]; j < lines[i + 1]; j++) {
-                linesSum[i] += numbers[j];
-                columnSum[j - lines[i]] += numbers[j];
-            }
-        }
-        
-        for (int i = 0; i < lineCount; i++) {
-            for (int j = lines[i]; j < lines[i + 1]; j++) {
-                System.out.print(linesSum[i] + columnSum[j - lines[i]] - numbers[j]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-        */
-
-        
-        /*
-        for (int i = lineCount; i > 0; i--) {
-            for (int j = lines[i] - 1; j >= lines[i - 1]; j--) {
-                System.out.print(numbers[j]);
-                System.out.print(" ");
-            }
-            System.out.print("\n");
-        }
-        */
-
     }
 }
